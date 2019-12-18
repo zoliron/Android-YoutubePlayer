90fe360683bb56fb5b12bff9226cb2430552cdcf
Ronen Zolicha <ronen1245@gmail.com>
2019-12-18 10:49:31 +0200

    Adding youtube api key

--- app/src/main/java/zoli/youtubeplayer/YoutubeActivity.java
+++ app/src/main/java/zoli/youtubeplayer/YoutubeActivity.java
@@ -1,17 +1,39 @@
 package zoli.youtubeplayer;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class YoutubeActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+
+    static final String GOOGLE_API_KEY = "AIzaSyBJz_p02OPb71aXgqXMBgnP6ke7mNhDfSk";
+    static final String YOUTUBE_VIDEO_ID = "roDz8mMvbIg";
+    static final String YOUTUBE_PLAYLIST = "PLknSwrodgQ72X4sKpzf5vT8kY80HKcUSe";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_youtube);
+//        setContentView(R.layout.activity_youtube);
+//        ConstraintLayout constraintLayout = findViewById(R.id.activity_youtube);
+        ConstraintLayout layout = (ConstraintLayout) getLayoutInflater().inflate(R.layout.activity_youtube, null);
+        setContentView(layout);
+
+//        Button button1 = new Button(this);
+//        button1.setLayoutParams(new ConstraintLayout.LayoutParams(300, 80));
+//        button1.setText("Button added");
+//        layout.addView(button1);
+
+        // Adding new youtube play to the layout
+        YouTubePlayerView player = new YouTubePlayerView(this);
+        player.setLayoutParams(new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        layout.addView(player);
     }
 
     @Override
